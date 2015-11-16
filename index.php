c14dfe68691daa43588bc9b2f48d0c2065c252cb
Petrov Sergey <serezha170796@mail.ru>
2015-11-16 14:56:56 +0300

    initial project version

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link href='https://fonts.googleapis.com/css?family=Jura&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="wrap_menu">
+			<div class="block_menu">
+				<div class="block_picture"></div>
+				<div class="menu">
+					<ul>
+						<li>Главная</li>
+						<li>Список заданий</li>
+						<li>Создать задание</li>
+						<li>Контакты</li>
+					</ul>
+				</div>
+				<div class="block_profile">
+					<div class="profile">
+						Фамилия Имя
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+		<div class="wrap_center_block">
+			<div class="center_block">
+				<div class="wrap_info">
+					<div class="info_block">
+						
+					</div>
+				</div>
+				<div class="wrap_registration">
+					<div class="registration_block">
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<h1>Популярные тесты</h1>
+
+
+		<div class="wrap_plitka_test">
+			<div class="plitka_test">
+				<div class="plitka">
+
+				</div>
+			</div>
+			<div class="plitka_test">
+				<div class="plitka">
+
+				</div>
+			</div>
+			<div class="plitka_test">
+				<div class="plitka">
+
+				</div>
+			</div>
+			<div class="plitka_test">
+				<div class="plitka">
+
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
